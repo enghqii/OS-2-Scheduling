c6da358350864a66446993b47a83b4b2dde5f458
ChanWoo Jeong <enghqii@gmail.com>
2014-10-12 04:46:09 +0900

    Error Messages

--- sched.c
+++ sched.c
@@ -12,17 +12,20 @@ typedef struct{
 } Process;
 
 FILE *	input;
+
 Process	processes[260];
+int num_proc = 0;
 
 int init_input(char * filename);
-void print_process(Process* process);
+void print_processes();
+int check_process(Process* process);
 
 int main(int argc, char ** argv)
 {
 	if(argc < 2)
 	{
-		printf("no input file\n");
-		return 0;
+		fprintf(stderr, "input file must specified\n");
+		return -1;
 	}
 	else
 	{
@@ -39,41 +42,97 @@ int init_input(char * filename)
 {
 	if(input = fopen(filename, "r"))
 	{
-		int 		i = 0;
 		char 	line[256];
 
-		for(i = 0; fgets(line, sizeof(line), input); i++)
+		num_proc = 0;
+
+		while(fgets(line, sizeof(line), input))
 		{
+
 			if(line[0] == '#')
 			{
 				continue;
 			}
 			else
 			{
-				sscanf(line,"%2s %d %d %d\n", 
-					processes[i].pid, 
-					&(processes[i].arrive_time), 
-					&(processes[i].service_time), 
-					&(processes[i].priority)
+				int ret = sscanf(line,"%2s %d %d %d\n", 
+					processes[num_proc].pid, 
+					&(processes[num_proc].arrive_time), 
+					&(processes[num_proc].service_time), 
+					&(processes[num_proc].priority)
 				);
 
-				print_process(&processes[i]);
+				if(ret < 4)
+				{
+					fprintf(stderr, "invalid format\n");
+					continue;
+				}
+
+				if(check_process(&processes[num_proc]) == false)
+				{
+					continue;
+				}
 			}
+
+			num_proc++;
 		}
+
+		print_processes();
 		return true;
 	}
 	else
 	{
+		fprintf(stderr, "failed to load input file \'%s\'\n", filename);
 		return false;
 	}
 }
 
-void print_process(Process* process)
+void print_processes()
 {
-	printf("pid : %s arrival : %d service : %d priority : %d\n", 
-		process->pid, 
-		process->arrive_time, 
-		process->service_time, 
-		process->priority
-	);
+	int i = 0;
+	for(i = 0; i < num_proc; i++){
+
+		Process* process = &processes[i];
+
+		printf("[%d] pid : %s arrival : %d service : %d priority : %d\n",
+			i, 
+			process->pid, 
+			process->arrive_time, 
+			process->service_time, 
+			process->priority
+		);
+	}
+}
+
+int check_process(Process* process)
+{
+	int i = 0;
+	for(i = 0; i < num_proc; i++)
+	{
+		if(strcmp(processes[i].pid, process->pid) == 0)
+		{
+			fprintf(stderr, "duplicate process id \'%s\'\n", process->pid);
+			return false;
+		}
+	}
+
+	if( 0 > process->arrive_time || process->arrive_time > 30 )
+	{
+		fprintf(stderr, "invalid arrive time \'%d\'\n", process->arrive_time);
+		return false;
+	}
+
+	if( 1 > process->service_time || process->service_time > 30 )
+	{
+		fprintf(stderr, "invalid service time \'%d\'\n", process->service_time);
+		return false;
+	}
+
+	if( 1 > process->priority || process->priority > 10 )
+	{
+		fprintf(stderr, "invalid priority \'%d\'\n", process->priority);
+		return false;
+	}
+
+	return true;
 }
